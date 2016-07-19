c86834aff46f1d099cc82372d3f356ea7c8554af
mat.gabi <mat.gabi@yahoo.com>
2016-07-19 15:17:11 +0300

    modified server for live reload

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/src')).listen(8080, function(){
-    console.log('[Server running on http://localhost:8080]');
+const fs = require('fs');
+const browserSync = require('browser-sync').create();
+
+const port = process.env.PORT || '4444';
+const host = process.env.HOST || '127.0.0.1';
+
+browserSync.init({
+    server: {
+        baseDir: __dirname,
+        index: 'src/index.html'
+    },
+    files: ['src/**/*.html','src/**/*.css','src/**/*.js'],
+    host: host,
+    port: port,
+    open: false,
+    notify: false,
+    ui: false,
+    ghostMode: false,
+    logLevel: 'silent'
 });
+browserSync.reload;
+f = fs.readFileSync(__dirname + '/help.txt', 'utf8');
+console.log(f.replace('{{host}}', host).replace('{{port}}', port));
+console.log('Server running http://%s:%d', host, port);
